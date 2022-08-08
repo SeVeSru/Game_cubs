c99fc8301b0d1262f9f091edd9f06a9c44be22c2
SeVeSru <qwertysp96@gmail.com>
2022-08-08 14:14:26 +0700

    Реализация поля игры

--- Game_cubs/Program.cs
+++ Game_cubs/Program.cs
@@ -8,8 +8,26 @@ namespace Game_cubs
 {
     internal class Program
     {
+        const int x = 25; //Размер поля
+        const int y = 25;
+        const string player1Str = "OO";//Метка игрока 1
+        const string player2Str = "XX";//Метка игрока 2
+        const string fieldStr = "##";//Метка пустого поля
+
         static void Main(string[] args)
         {
+            int[,] Pole = new int[x, y];
+            Random random = new Random();
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Pole[i, j] = 0;
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
