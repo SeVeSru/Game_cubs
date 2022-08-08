c855159eb6f9ff5fe7e7e03388b6b19f884b2fd5
SeVeSru <qwertysp96@gmail.com>
2022-08-08 15:55:05 +0700

    Реализация кубиков и хождение игроков

--- Game_cubs/Program.cs
+++ Game_cubs/Program.cs
@@ -10,10 +10,11 @@ namespace Game_cubs
     {
         const int x = 25; //Размер поля
         const int y = 25;
-        const string player1Str = "OO";//Метка игрока 1
-        const string player2Str = "XX";//Метка игрока 2
-        const string fieldStr = "##";//Метка пустого поля
+        const string player1Str = "O";//Метка игрока 1
+        const string player2Str = "X";//Метка игрока 2
+        const string fieldStr = "#";//Метка пустого поля
         static int[,] Pole = new int[x, y];
+        static int round = 2;
 
         static void Main(string[] args)
         {
@@ -26,9 +27,20 @@ namespace Game_cubs
                     Pole[i, j] = 0;
                 }
             }
-            PoleOut();
+            //PoleOut();
 
-            Console.ReadKey();
+            
+            do
+            {
+                if (round == 1)
+                    round++;
+                else round = 1;
+
+                int x = random.Next(1, 7), y = random.Next(1, 7);
+                InterfaceMove(round, x, y);
+                Console.ReadKey();
+
+            } while (true);
         }
         static void PoleOut()
         {
@@ -45,6 +57,37 @@ namespace Game_cubs
                 }
                 Console.WriteLine();
             }
+        }
+
+        static void InterfaceMove(int player, int rx, int ry)
+        {
+            int x1, y1;
+            Console.Clear();
+
+            PoleOut();
+            Console.WriteLine("Вам выпало: " + rx + ", и " + ry);
+
+            Console.Write("Выберите позицию: \nx(Вертикально): ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y(Горизантально): ");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            if (player == 1)
+                for (int i = x1; i < x1 + rx; i++)
+                {
+                    for (int j = y1; j < y1 + ry; j++)
+                    {
+                        Pole[i, j] = player;
+                    }
+                }
+            else if (player == 2)
+                for (int i = x1; i < x1 + rx; i++)
+                {
+                    for (int j = y1; j < y1 + ry; j++)
+                    {
+                        Pole[x-1-i, y-1-j] = player;
+                    }
+                }
         }
     }
 }
